c2c615ddd220026b6856d02b247f6a30460b2087
alicia <nataly-tm2003@outlook.com>
2026-05-24 11:21:53 -0500

    Agrego carpeta

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+/*
+@Autor: Nataly Tituaña
+@Fecha: 22/02/2026
+@Descripción: Planeación de paquetes en Go
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Sistema de Gestión de E-commerce")
+}
